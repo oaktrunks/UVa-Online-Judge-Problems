c65f10fa4a9b8fcca079225fd845e6546d6f705c
oaktrunks <TomeiDLT@gmail.com>
2017-10-09 10:04:53 -0500

    Completed uva11498

--- solved/uva11498.cpp
+++ solved/uva11498.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 
+using namespace std;
 int main() {
-	
-	int K, N, M, X,Y;
+
+	int  K, N, M, X, Y;
+	ios_base::sync_with_stdio(false); cin.tie(0);
+
+	while (cin >> K) {
+		//cout << "in main while loop with K = " << K << endl;
+		if (K == 0) break;
+		cin >> N >> M; //Coordinates of divisor
+		//cout << "N = " << N << "  M = " << M << endl;
+		for (int i = 0; i < K; i++) {
+			//cout << "in test cases with K = " << K << endl;
+			cin >> X >> Y;
+			//cout << "X = " << X << "  Y = " << Y << endl;
+			if (X ==  N || Y == M) {
+				cout << "divisa\n";
+			}
+			else {
+				if (Y > M) {
+					cout << 'N';
+				}
+				else {
+					cout << 'S';
+				}
+
+				if (X > N) {
+					cout << "E\n";
+				}
+				else {
+					cout << "O\n";
+				}
+			}
+		}
+	}
 
 	return 0;
 }
